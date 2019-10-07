35962b5e2e4dac9a1b4888b15a8a2f022e3bce8d
Kim McCaskill <kimmccaskill92@gmail.com>
2019-10-06 18:59:06 -0600

    Delete code that didnt work

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-// function insertName(dogName) {
-//
-// };
 var insertName = document.querySelector('.name-input')
 var submit = document.querySelector('.name-submit')
 var someDogText = document.querySelector('.some-dogs')
-
-
-// submit.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   someDogText.innerHTML = insertName.value;
-// });
-// form refreshing after submitting  //
 
 submit.addEventListener('click', function() {
   someDogText.innerHTML = insertName.value;
